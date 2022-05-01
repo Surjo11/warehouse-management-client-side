cf349645eddc5a89fe5707d803867767dcc48f4f
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-05-01 14:36:11 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blogs from "./components/pages/Blogs/Blogs";
+import Home from "./components/pages/Home/Home";
+import Header from "./components/shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="blogs" element={<Blogs></Blogs>} />
+      </Routes>
     </div>
   );
 }
